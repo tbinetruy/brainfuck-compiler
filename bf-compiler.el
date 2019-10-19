c0823054da92a296aabb46c6cfce8676a953caa4
Thomas Binétruy <tbinetruy@gmail.com>
2019-10-19 22:31:08 +0200

    added - instruction

--- bf-compiler.el
+++ bf-compiler.el
@@ -8,8 +8,17 @@
 (defmacro push-instruction (instr)
   `(setq instructions (nconc instructions (list ,instr))))
 
-(defun compiler//+ (instructions)
-  "The steps are as follows:
+(defun compiler//init-code (instructions)
+  "Initialize the program counter, stored in ecx, to 0."
+  (push-instruction '("PUSH" 0))
+  (push-instruction '("PUSH" ecx))
+  (push-instruction '("STORE"))
+  instructions)
+
+(defun compiler//increment-value (instructions val)
+  "Increments by val the content of the address pointed at by the pc.
+
+The steps are as follows:
 - Get the program counter from ecx and store it to eax.
   This is the address at which we want to increment the value.
 - We then read from the ram at the address in eax (the pc)
@@ -28,7 +37,7 @@
   (push-instruction '("PUSH" eax))
   (push-instruction '("LOAD"))
 
-  (push-instruction '("PUSH" 1))
+  (push-instruction `("PUSH" ,val))
   (push-instruction '("ADD"))
 
   (push-instruction '("PUSH" ebx))
@@ -42,27 +51,23 @@
   (push-instruction '("WRITE_RAM"))
   instructions)
 
-(defun compiler//init-code (instructions)
-  "Initialize the program counter, stored in ecx, to 0."
-  (push-instruction '("PUSH" 0))
-  (push-instruction '("PUSH" ecx))
-  (push-instruction '("STORE"))
-  instructions)
-
 (defun compiler//compile (code)
   (let ((tokens (lexer//lex code))
         (instructions '()))
     (setq instructions (compiler//init-code instructions))
     (dolist (el tokens)
+      (if (equal el "-")
+          (setq instructions (compiler//increment-value instructions -1)))
       (if (equal el "+")
-          (setq instructions (compiler//+ instructions))))
+          (setq instructions (compiler//increment-value instructions 1))))
     instructions))
 
 (message "%s" (lexer//lex "++>>"))
 (message "%s" (compiler//compile "+"))
 
+(message "%s" (compiler//compile "++-"))
 
-(message "%s" (vm//main (compiler//compile "++") t))
+(message "%s" (vm//main (compiler//compile "++-") t))
 
 ;(message "%s" (vm//main '(("PUSH" 3)
 ;                          ("PUSH" eax)
